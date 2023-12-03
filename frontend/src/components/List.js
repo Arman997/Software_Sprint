a23b0d8d86e44c8c3986bdc1183e9a40506f54c0
Arman <iperov.55@gmail.com>
2023-12-03 10:59:16 +0400

    test

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import './styleList.css';
 
 const List = () => {
     return (
-        <div>
+        <div className='list'>
+            
+            <h2>innovationItTech</h2>
+
+            <ul  className='unordered_list'>
+                <li>Կենտրոն</li>
+                <li>Աջափնյակ</li>
+                <li>Մալաթիա-Սեբաստիա</li>
+                <li>Դավթաշեն</li>
+                <li>Նոր Նորք</li>
+                <li>նորք-Մարաշ</li>
+                <li>Քանաքեռ-Զեյթուն</li>
+                <li>Ավան</li>
+                <li>Արաբկիր</li>
+                <li>Շենգավիթ</li>
+                <li>Էրեբունի</li>
+                <li>Նուբարաշեն</li>
+            </ul>
             
         </div>
     );
